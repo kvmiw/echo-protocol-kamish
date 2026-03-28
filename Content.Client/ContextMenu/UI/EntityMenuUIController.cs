388e1800de5e06c1b604167c9ccb99b20c745179
FaDeOkno <logkedr18@gmail.com>
2026-03-28 09:00:51 +0400

    add(src, ui): Настройка для включения/выключения иконок сущностей

--- Content.Client/ContextMenu/UI/EntityMenuUIController.cs
+++ Content.Client/ContextMenu/UI/EntityMenuUIController.cs
@@ -291,6 +291,8 @@ namespace Content.Client.ContextMenu.UI
         private void AddEntityToMenu(EntityUid entity, ContextMenuPopup menu)
         {
             var element = new EntityMenuElement(entity);
+
+            element.Icon.Visible = _cfg.GetCVar(EchoCCVars.EntityMenuIcons);    // ECHO-Tweak: скрытые иконки
             element.SubMenu = new ContextMenuPopup(_context, element);
             element.SubMenu.OnPopupOpen += () => _verb.OpenVerbMenu(entity, popup: element.SubMenu);
             element.SubMenu.OnPopupHide += element.SubMenu.MenuBody.RemoveAllChildren;
--- Resources/Locale/ru-RU/_ECHO/escape-menu/options-menu.ftl
+++ Resources/Locale/ru-RU/_ECHO/escape-menu/options-menu.ftl
@@ -1,2 +1,3 @@
 ui-options-film-grain = Grain эффект
 ui-options-show-offer-mode-indicators = Показывать индикатор передачи предмета
+ui-options-entity-menu-icons = Показывать иконки сущности в контекстном меню
